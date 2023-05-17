641671dc0b8a1ecf3813fbb2b09a31737b53f902
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-05-17 09:20:04 +0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Ravshanbek Tursunbaev on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
